4478c4a6830e9669b056d03ade853a701fd1847d
ZeyuKeithFu <zeyufu@bu.edu>
2019-11-01 15:35:25 -0400

    Divide into MVP.

--- app/src/main/java/com/killerwhale/memary/Activity/MainActivity.java
+++ app/src/main/java/com/killerwhale/memary/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.killerwhale.memary;
+package com.killerwhale.memary.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.killerwhale.memary.R;
 
 public class MainActivity extends AppCompatActivity {
 
